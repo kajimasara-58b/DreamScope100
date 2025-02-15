abcb36062b74cc2c860d2489f481cc428157734f
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-02-15 14:37:07 +0900

    レイアウト修正③

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root 'home#index'
-  get 'login', to: 'sessions#new'
-  #/loginにアクセスするとSessionsControllerのnewアクションがよばれる
+  root "home#index"
+  get "login", to: "sessions#new"
+  # /loginにアクセスするとSessionsControllerのnewアクションがよばれる
 end
