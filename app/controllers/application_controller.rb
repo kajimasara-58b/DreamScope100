d4778f5f1539f570367611ec2e4a45ccde65964b
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-02-22 23:09:25 +0900

    Userテーブルにnameカラム追加・ユーザー登録機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,5 +12,12 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     # ログイン状態をチェックする処理
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
