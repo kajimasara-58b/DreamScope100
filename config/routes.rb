201e1e2a50671d4847882e1970b64f7e32bf593e
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-02-22 22:25:38 +0900

    目標一覧画面たたき作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   }
   get "dashboard", to: "dashboard#index"
   get "users/show", to: "users#show"
+  resources :goals
   # get "login", to: "sessions#new"
   # /loginにアクセスするとSessionsControllerのnewアクションがよばれる
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
