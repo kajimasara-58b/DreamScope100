241a52a5792f41408cdeafcf25da8ab447d7433d
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-02-23 23:47:40 +0900

    目標作成機能の実装・フラッシュメッセージの実装

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,8 +1,25 @@
 class GoalsController < ApplicationController
   def new
+    @goal = Goal.new
   end
 
   def index
     @goals = Goal.all
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to goals_path, notice: '目標を作成しました'
+    else
+      flash[:alert] = '目標の作成に失敗しました。入力内容を確認してください。'
+      redirect_to new_goal_path
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:title, :due_date, :status) # 必要な属性を指定すること
   end
 end
