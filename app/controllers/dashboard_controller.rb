0c2ad4246f791c8ea2640fa3cdc35cb55a53e880
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-02-25 08:38:30 +0900

    自分の目標しか見られないように修正

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    @achieved_goals = Goal.where(status: "済").count
+    @achieved_goals = Goal.where(status: "済", user_id: current_user.id).count
   end
 end
