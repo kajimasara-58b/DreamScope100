d6c11f8ab4d52d80eb8671116cacc0f0083b3e71
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-03-30 20:27:18 +0900

    パスワード変更機能の実装完了

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -20,9 +20,11 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # PUT /resource/password
-  # def update
-  #   super
-  # end
+  def update
+    sign_out(resource_name) # リソースをログアウトさせる
+    redirect_to new_user_session_path
+
+  end
 
   # protected
 
