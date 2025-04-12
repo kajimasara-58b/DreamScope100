b390a7d252667f7b0a6f2a29034089ca92e57f66
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-04-12 11:07:52 +0900

    画像生成ボタン正常表示まで

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,14 +1,14 @@
 class ImagesController < ApplicationController
-  def generate
+  def generate_image
     goals = fetch_goals # 目標を取得するメソッドを呼び出す
     image = create_image(goals) # 画像を生成するメソッドを呼び出す
-    send_data image.to_blob, filename: 'output.jpg', type: 'image/jpeg', disposition: 'attachment' # 画像を返す
+    send_data image.to_blob, filename: "output.jpg", type: "image/jpeg", disposition: "attachment" # 画像を返す
   end
 
   private
 
   def fetch_goals
-    Goal.limit(100) # 目標を100件取得
+    Goal.all # 目標を100件取得
   end
 
   def create_image(goals)
@@ -23,7 +23,7 @@ class ImagesController < ApplicationController
       x = (index % 2) * 300 # x座標の計算
       y = (index / 2) * 50   # y座標の計算
       image.combine_options do |c|
-        c.draw "text #{x},#{y} '#{goal.message}'"
+        c.draw "text #{x},#{y} '#{goal.title}'"
       end
     end
 
