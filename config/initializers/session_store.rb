b8968be051849ec1fdebb768133da7d4f8173031
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-05-02 17:22:58 +0900

    レイアウト修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
-  key: '_dreamscope100_session',
+  key: "_dreamscope100_session",
   secure: false,
   httponly: true,
   expire_after: 1.day,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'test', to: 'test#index'
+  get "test", to: "test#index"
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations",
