4a5f0a727d3d689b8e971507a9c4215b43249fb2
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-05-11 16:15:03 +0900

    LINE通知機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_11_053849) do
     t.text "message", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
