1df9403520ddf15a9c63c6fce7d8e3ce3fab45c1
梶間紗羅 <e5mt2ny7s@gmail.com>
2025-06-01 17:08:43 +0900

    レイアウト修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:dashboard, :line_friend_add] # ログイン必須
+  before_action :authenticate_user!, only: [ :dashboard, :line_friend_add ] # ログイン必須
 
   def welcome
     @line_friend_link = "line://ti/p/@946voeaz" # 実際の公式アカウントIDに置き換え
   end
-  
+
   def dashboard
   end
 
